65474de210fcff2d45f49e0063035e64f363e47b
Rama Chandra <sriramsrinivasan@srirams-macbook-pro.local>
2016-02-09 13:24:31 +0000

    Initial commint

--- tvOSApp/ViewController.swift
+++ tvOSApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,48 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "Item \(indexPath.row + 1)"
+        cell.detailTextLabel?.text = "Subtitle \(indexPath.row + 1)"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        print("\(indexPath.row) selected")
+    }
+    
+    override var preferredFocusedView:UIView? {
+        
+        return self.view.subviews[0]
+    }
+    
+    func tableView(tableView: UITableView, canFocusRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        
+        if indexPath.row == 1 {
+            
+            return false
+        }
+        
+        
+        return true
+        
+        
     }
 
 
